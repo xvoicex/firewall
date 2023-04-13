706e207d3781ef8d9d25c98607d1a8588070651e
xvoicex <xvoicex@outlook.com>
2023-04-13 12:51:50 +0800

    2023-04-13 12:51:50

--- firewall.sh
+++ firewall.sh
@@ -1,10 +1,10 @@
 #/bin/bash
 time=$(date +%F@%T)
 echo -e 删除6天前的日志...
-find /root/firewall/ -mtime +30 -name '*.log' | xargs rm -f
+find /root/firewall/log/ -mtime +30 -name '*.log' | xargs rm -f
 echo -e 保存lastb日志...
-lastb > /root/firewall/lastb-$time.log 
-file=/root/firewall/$time.log
+lastb > /root/firewall/log/lastb-$time.log 
+file=/root/firewall/log/$time.log
 lastb |awk '{printf $3 "\n"}' |awk '{++S[$NF]} END{for (a in S)print a,S[a]}' |awk '{if($2>10)print $0}' |sort -nrk2 |awk '{printf $1 "\n"}'  >  $file
 lines=$(cat $file |wc -l)
 echo -e 一共 $lines 个IP
@@ -20,7 +20,7 @@ do
                 fi
 done
 echo -e 发送邮件...
-mail -a /root/firewall/lastb-$time.log -s "N1_firewall" vx@vxoice.onmicrosoft.com < $file
+mail -a /root/firewall/log/lastb-$time.log -s "N1_firewall" vx@vxoice.onmicrosoft.com < $file
 echo -e 清空btmp文件...
 echo > /var/log/btmp
 echo -e 更新防火墙规则...
@@ -29,4 +29,4 @@ c=$(firewall-cmd --list-rich-rules |grep ipv4 |awk '{printf $4 "\n"}' |sed 's/ad
 echo -e 共$c条,禁用IP列表：
 firewall-cmd --list-rich-rules |grep ipv4 |awk '{printf $4 "\n"}' |sed 's/address=//g;s/"//g' > /root/firewall/README.md
 cat README.md
-su root -c 'cd /root/firewall/ && git add README.md && git commit -m "$time" && git push'
+#su root -c 'cd /root/firewall/ && git add README.md && git commit -m "$time" && git push'
